1edf37de5fca9f4afd8b6db59a3480a0e8560cd7
Alex Nou <alexnou@gmail.com>
2021-07-17 15:01:32 -0700

    Fix client options

--- src/classes/MongoConnector.ts
+++ src/classes/MongoConnector.ts
@@ -1,28 +1,23 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
+
+const defaultMongoClientOptions: MongoClientOptions = {};
 
 export default class MongoConnector {
   private static mongodbClient: MongoClient;
 
-  static async getMongoDbClient() {
+  static async getMongoDbClient(options?: MongoClientOptions) {
     if (this.mongodbClient) {
       return this.mongodbClient;
     } else {
-      return await this.connectToMongo()
+      return await this.connectToMongo(options)
     }
   }
 
-  private static async connectToMongo() {
-    const mongoClientOptions = {
-      poolSize: 50,
-      keepAlive: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
+  private static async connectToMongo(options?: MongoClientOptions) {
     if (process.env.MONGO_CONNECTION_URL) {
-      return await MongoClient.connect(process.env.MONGO_CONNECTION_URL, mongoClientOptions);
+      return await MongoClient.connect(process.env.MONGO_CONNECTION_URL, options ?? defaultMongoClientOptions);
     } else {
       throw new Error("MONGO_CONNECTION_URL environment variable not set. Cannot connect to DB.")
     }
